63b36c6bc16d44057f73b9ebb6a5fd875bc71d28
XintaoYu <2019202446@ruc.edu.cn>
2024-10-05 11:23:22 +0800

    update lean version

--- .lake/build/ir/PadicLFunctions4.c
+++ .lake/build/ir/PadicLFunctions4.c
@@ -1,6 +1,6 @@
 // Lean compiler output
-// Module: PadicLFunctions4.nonarch
-// Imports: Init PadicLFunctions4.ZModProp Mathlib.Data.ZMod.Algebra
+// Module: PadicLFunctions4
+// Imports: Init PadicLFunctions4.Basic
 #include <lean/lean.h>
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wunused-parameter"
@@ -14,20 +14,16 @@
 extern "C" {
 #endif
 lean_object* initialize_Init(uint8_t builtin, lean_object*);
-lean_object* initialize_PadicLFunctions4_ZModProp(uint8_t builtin, lean_object*);
-lean_object* initialize_Mathlib_Data_ZMod_Algebra(uint8_t builtin, lean_object*);
+lean_object* initialize_PadicLFunctions4_Basic(uint8_t builtin, lean_object*);
 static bool _G_initialized = false;
-LEAN_EXPORT lean_object* initialize_PadicLFunctions4_nonarch(uint8_t builtin, lean_object* w) {
+LEAN_EXPORT lean_object* initialize_PadicLFunctions4(uint8_t builtin, lean_object* w) {
 lean_object * res;
 if (_G_initialized) return lean_io_result_mk_ok(lean_box(0));
 _G_initialized = true;
 res = initialize_Init(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
-res = initialize_PadicLFunctions4_ZModProp(builtin, lean_io_mk_world());
-if (lean_io_result_is_error(res)) return res;
-lean_dec_ref(res);
-res = initialize_Mathlib_Data_ZMod_Algebra(builtin, lean_io_mk_world());
+res = initialize_PadicLFunctions4_Basic(builtin, lean_io_mk_world());
 if (lean_io_result_is_error(res)) return res;
 lean_dec_ref(res);
 return lean_io_result_mk_ok(lean_box(0));
